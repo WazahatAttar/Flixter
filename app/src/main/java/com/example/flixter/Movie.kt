ff2123650c3a836cd57f8b4a371c00e6120d541b
Wazahat-Attar <wattar@horizon.csueastbay.edu>
2022-03-04 22:19:23 -0800

    Part 2 update

--- app/src/main/java/com/example/flixter/Movie.kt
+++ app/src/main/java/com/example/flixter/Movie.kt
@@ -1,8 +1,13 @@
 package com.example.flixter
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
-data class Movie(val movieId: Int, private val posterPath: String, val title: String, val overview: String,) {
+@Parcelize
+data class Movie(val movieId: Int, val voteAverage: Double, private val posterPath: String, val title: String, val overview: String,) : Parcelable {
+    @IgnoredOnParcel
     val posterUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
     companion object{
         fun fromJsonArray(movieArray: JSONArray?): List<Movie> {
@@ -13,6 +18,7 @@ data class Movie(val movieId: Int, private val posterPath: String, val title: St
                     movies.add(
                         Movie(
                             movieJson.getInt("id"),
+                            movieJson.getDouble("vote_average"),
                             movieJson.getString("poster_path"),
                             movieJson.getString("title"),
                             movieJson.getString("overview")
--- app/src/main/java/com/example/flixter/MovieAdapter.kt
+++ app/src/main/java/com/example/flixter/MovieAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.flixter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -27,15 +29,29 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
         private val movietitle=itemView.findViewById<TextView>(R.id.movietitle)
         private val movieid=itemView.findViewById<ImageView>(R.id.poster)
         private val movieoverview=itemView.findViewById<TextView>(R.id.movieoverview)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
         fun bind(movie: Movie){
             movietitle.text = movie.title
             movieoverview.text = movie.overview
             Glide.with(context).load(movie.posterUrl).into(movieid)
 
+        }
+
+        override fun onClick(p0: View?) {
+            val movie = movies[adapterPosition]
+            Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra("MOVIE_EXTRA" , movie)
+            context.startActivity(intent)
         }
     }
 }
